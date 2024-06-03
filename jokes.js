24ec9d89dbb7759ba8b3a0f510629970a596a859
Gagan Suman <ilovescience584@gmail.com>
2024-06-03 17:43:55 +0530

    first commit

---
+++ jokes.js
@@ -0,0 +1,26 @@
+let jokepara = document.querySelector("#joke-para")
+const resetJoke=document.querySelector("#reset-joke")
+const apiUrl="https://v2.jokeapi.dev/joke/Any?blacklistFlags=nsfw&type=single"
+
+// jokepara.style.display="none"
+
+async function ShowJoke(){
+
+    let response= await fetch(apiUrl)
+    const apiJoke=await response.json()
+    console.log(apiJoke.joke)
+    
+    jokepara.innerHTML=apiJoke.joke
+
+    resetJoke.addEventListener("click",async ()=>{
+         response= await fetch(apiUrl)
+         const apiJoke=await response.json()
+         console.log(apiJoke.joke)
+    
+        jokepara.innerHTML=apiJoke.joke
+
+    })
+}
+
+ShowJoke();
+
